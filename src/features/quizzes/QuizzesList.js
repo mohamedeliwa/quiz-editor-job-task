29408423e1d45d4a72649645a566af7c2eb0ab0c
mohamedeliwa <mohamed.av11@gmail.com>
2022-03-20 18:58:47 +0200

    rearranging rendered quizzes

--- src/features/quizzes/QuizzesList.js
+++ src/features/quizzes/QuizzesList.js
@@ -1,23 +1,27 @@
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const QuizzesList = () => {
-  const quizzes = useSelector((state) => state.quizzes)
+  const quizzes = useSelector((state) => state.quizzes);
 
-  const renderedquizzes = quizzes.map((quiz) => (
-    <article className="quiz-excerpt" key={quiz.id}>
-      <h3>{quiz.title}</h3>
-      <p className="quiz-content">{quiz.questions_answers.length} questions</p>
-      <Link to={`/quizzes/${quiz.id}`} className="button muted-button">
-        Take Quiz
-      </Link>
-    </article>
-  ))
+  const renderedquizzes = quizzes
+    .map((quiz) => (
+      <article className="quiz-excerpt" key={quiz.id}>
+        <h3>{quiz.title}</h3>
+        <p className="quiz-content">
+          {quiz.questions_answers.length} questions
+        </p>
+        <Link to={`/quizzes/${quiz.id}`} className="button muted-button">
+          Take Quiz
+        </Link>
+      </article>
+    ))
+    .reverse();
 
   return (
     <section className="quizzes-list">
       <h2>Quizzes List</h2>
       {renderedquizzes}
     </section>
-  )
-}
+  );
+};
